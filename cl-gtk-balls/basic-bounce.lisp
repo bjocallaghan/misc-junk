bca6fd679f9a3ec4344deed9c1db43129f887a40
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2015-03-28 20:57:53 -0500

    working animation with gravity

--- cl-gtk-balls/basic-bounce.lisp
+++ cl-gtk-balls/basic-bounce.lisp
@@ -6,16 +6,18 @@
 
 (defparameter +num-balls+ 150)
 
-(defparameter +canvas-width+ 600)
-(defparameter +canvas-height+ 400)
+(defparameter +canvas-width+ 800)
+(defparameter +canvas-height+ 600)
 (defparameter +target-fps+ 60.0)
 (defparameter +time-step+ (/ +target-fps+))
 
 (defparameter +max-dx+ (* 1/2 +canvas-width+))
 (defparameter +max-dy+ (* 1/2 +canvas-height+))
 
-(defparameter +min-radius+ 2)
-(defparameter +max-radius+ 20)
+(defparameter +min-radius+ 8)
+(defparameter +max-radius+ 25)
+
+(defparameter +g-constant+ -1000)
 
 (defun random-range (lower upper)
   (+ lower (random (- upper lower))))
@@ -24,7 +26,7 @@
   (let ((day-names '("Monday" "Tuesday" "Wednesday"
                      "Thursday" "Friday" "Saturday" "Sunday")))
     (multiple-value-bind
-        (second minute hour date month year day-of-week dst-p tz)
+          (second minute hour date month year day-of-week dst-p tz)
         (get-decoded-time)
       (declare (ignore dst-p))
       (format nil "~a ~d/~2,'0d/~d ~2,'0d:~2,'0d:~2,'0d (GMT~@d)"
@@ -33,40 +35,36 @@
 
 (defun make-ball ()
   (flet ((in-bounds-p (ball)
-                      (and (>= (getf ball :x) (getf ball :radius))
-                           (<= (getf ball :x) (- +canvas-width+ (getf ball :radius)))
-                           (>= (getf ball :y) (getf ball :radius))
-                           (<= (getf ball :y) (- +canvas-height+ (getf ball :radius)))))
+           (and (>= (getf ball :x) (getf ball :radius))
+                (<= (getf ball :x) (- +canvas-width+ (getf ball :radius)))
+                (>= (getf ball :y) (getf ball :radius))
+                (<= (getf ball :y) (- +canvas-height+ (getf ball :radius)))))
          (make-ball ()
-                    (list :time-step +time-step+
-                          :radius (random-range +min-radius+ +max-radius+)
-                          :x (random +canvas-width+)
-                          :y (random +canvas-height+)
-                          :dx (random-range (- +max-dx+) +max-dx+)
-                          :dy (random-range (- +max-dy+) +max-dy+)
-                          :color (random-color))))
+           (let* ((radius (random-range +min-radius+ +max-radius+))
+                  (y (+ (random +canvas-height+) radius))
+                  (dy (random-range (- +max-dy+) +max-dy+)))
+             (list :time-step +time-step+
+                   :radius radius
+                   :x (random +canvas-width+)
+                   :y y
+                   :dx (random-range (- +max-dx+) +max-dx+)
+                   :dy dy
+                   :max-speed (sqrt (* 2 (+ (abs (* +g-constant+ (- y radius)))
+                                            (* 0.5 dy dy))))
+                   :color (random-color)))))
     (do ((ball (make-ball) (make-ball)))
         ((in-bounds-p ball) ball))))
 
 (defun update-ball (ball)
   (incf (getf ball :x) (* (getf ball :dx) +time-step+))
-  (incf (getf ball :y) (* (getf ball :dy) +time-step+))
   (when (or (< (getf ball :x) (getf ball :radius))
             (> (getf ball :x) (- +canvas-width+ (getf ball :radius))))
     (setf (getf ball :dx) (- (getf ball :dx))))
-  (when (or (< (getf ball :y) (getf ball :radius))
-            (> (getf ball :y) (- +canvas-height+ (getf ball :radius))))
-    (setf (getf ball :dy) (- (getf ball :dy)))))
-
-    ;;     self.dy += G_CONSTANT * self.time_step
-    ;;     self.y += (self.dy * self.time_step)
-    ;;     if (self.y-self.radius) < 0:
-    ;;         self.y = self.radius
-    ;;         self.temp_max_speed *= .8
-    ;;         if self.temp_max_speed < .2 * self.max_speed:
-    ;;             self.temp_max_speed = self.max_speed
-    ;;         self.dy = self.temp_max_speed
-
+  (incf (getf ball :dy) (* +g-constant+ +time-step+))
+  (incf (getf ball :y) (* (getf ball :dy) +time-step+))
+  (when (< (getf ball :y) (getf ball :radius))
+    (setf (getf ball :dy) (getf ball :max-speed))
+    (setf (getf ball :y) (getf ball :radius))))
 
 (defun draw-ball (ball cr)
   (apply #'cairo-set-source-rgb (cons cr (getf ball :color)))
@@ -80,13 +78,13 @@
   (cairo-stroke cr))
 
 (defun random-color ()
-  (flet ((rand-val ()
-                   (/ (float (random 100)) 100)))
-    (list (rand-val) (rand-val) (rand-val))))
+  (let ((base (list (random 1.0) (random 1.0) (random 1.0))))
+    (mapcar (lambda (x) (/ x (apply #'max base))) base)))
 
 (defun bounce-main ()
   (within-main-loop
-   (let ((balls (loop repeat +num-balls+ collect (make-ball)))
+   (let ((runningp nil)
+         (balls (loop repeat +num-balls+ collect (make-ball)))
          (window (make-instance 'gtk-window
                                 :type :toplevel
                                 :title "Bouncing Balls"))
@@ -94,17 +92,21 @@
                                 :width-request +canvas-width+
                                 :height-request +canvas-height+))
          (box (gtk-box-new :vertical 2))
-         (step-button (gtk-button-new-with-label "Step"))
+         (run-button (gtk-button-new-with-label "Start"))
          (add-button (gtk-button-new-with-label "Add")))
-     (labels ((update (widget)
-                      (declare (ignore widget))
-                      (dolist (ball balls) (update-ball ball))
-                      (gtk-widget-queue-draw canvas)
-                      t))
+     (labels ((update (&optional widget)
+                (declare (ignore widget))
+                (dolist (ball balls) (update-ball ball))
+                (gtk-widget-queue-draw canvas)
+                t))
        (g-signal-connect window "destroy" (lambda (widget)
                                             (declare (ignore widget))
                                             (leave-gtk-main)))
-       (g-signal-connect step-button "clicked" #'update)
+       (g-signal-connect run-button "clicked"
+                         (lambda (widget)
+                           (declare (ignore widget))
+                           (g-timeout-add (floor (* 1000 +time-step+))
+                                          #'update)))
        (g-signal-connect add-button "clicked"
                          (lambda (widget)
                            (declare (ignore widget))
@@ -123,6 +125,6 @@
                              t)))
        (gtk-container-add window box)
        (gtk-box-pack-start box canvas)
-       (gtk-box-pack-start box step-button)
+       (gtk-box-pack-start box run-button)
        (gtk-box-pack-start box add-button)
        (gtk-widget-show-all window)))))
